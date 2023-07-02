ebea99fc2068598f0e7a497717855a7196727b1f
WeZZard <me@wezzard.com>
2023-07-03 00:13:33 +0800

    Update source files and adds NOTICE file for codes ported from the Swift open source project.

---
+++ Sources/COWMacros/Extensions.swift
@@ -0,0 +1,240 @@
+//===------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===------------------------------------------------------------------===//
+
+import SwiftSyntax
+
+extension StructDeclSyntax {
+  
+  internal func hasMacroApplication(_ name: String) -> Bool {
+    guard let attributes else {
+      return false
+    }
+    for each in attributes where each.hasName(name) {
+      return true
+    }
+    return false
+  }
+  
+}
+
+extension VariableDeclSyntax {
+  
+  internal var identifierPattern: IdentifierPatternSyntax? {
+    bindings.first?.pattern.as(IdentifierPatternSyntax.self)
+  }
+  
+  internal var identifier: TokenSyntax? {
+    identifierPattern?.identifier
+  }
+  
+  internal func hasMacroApplication(_ name: String) -> Bool {
+    guard let attributes else {
+      return false
+    }
+    for each in attributes where each.hasName(name) {
+      return true
+    }
+    return false
+  }
+  
+  internal func firstMacroApplication(_ name: String) -> AttributeSyntax? {
+    guard let attributes else {
+      return nil
+    }
+    for each in attributes where each.hasName(name) {
+      switch each {
+      case .attribute(let attrSyntax):
+        return attrSyntax
+      case .ifConfigDecl:
+        break
+      }
+    }
+    return nil
+  }
+  
+  internal var isInstance: Bool {
+    if let modifiers {
+      for modifier in modifiers {
+        for token in modifier.tokens(viewMode: .all) {
+          if token.tokenKind == .keyword(.static) || token.tokenKind == .keyword(.class) {
+            return false
+          }
+        }
+      }
+    }
+    return true
+  }
+  
+  internal func isEquivalent(to other: VariableDeclSyntax) -> Bool {
+    if isInstance != other.isInstance {
+      return false
+    }
+    return identifier?.text == other.identifier?.text
+  }
+  
+}
+
+extension TypeSyntax {
+  
+  internal func genericSubstitution(_ parameters: GenericParameterListSyntax?) -> String? {
+    var genericParameters = [String : TypeSyntax?]()
+    if let parameters {
+      for parameter in parameters {
+        genericParameters[parameter.name.text] = parameter.inheritedType
+      }
+    }
+    var iterator = self.asProtocol(TypeSyntaxProtocol.self).tokens(viewMode: .sourceAccurate).makeIterator()
+    guard let base = iterator.next() else {
+      return nil
+    }
+    
+    if let genericBase = genericParameters[base.text] {
+      if let text = genericBase?.identifier {
+        return "some " + text
+      } else {
+        return nil
+      }
+    }
+    var substituted = base.text
+    
+    while let token = iterator.next() {
+      switch token.tokenKind {
+      case .leftAngle:
+        substituted += "<"
+      case .rightAngle:
+        substituted += ">"
+      case .comma:
+        substituted += ","
+      case .identifier(let identifier):
+        let type: TypeSyntax = "\(raw: identifier)"
+        guard let substituedType = type.genericSubstitution(parameters) else {
+          return nil
+        }
+        substituted += substituedType
+        break
+      default:
+        // ignore?
+        break
+      }
+    }
+    
+    return substituted
+  }
+  
+}
+
+extension FunctionDeclSyntax {
+  
+  internal var isInstance: Bool {
+    if let modifiers {
+      for modifier in modifiers {
+        for token in modifier.tokens(viewMode: .all) {
+          if token.tokenKind == .keyword(.static) || token.tokenKind == .keyword(.class) {
+            return false
+          }
+        }
+      }
+    }
+    return true
+  }
+  
+  internal struct SignatureStandin: Equatable {
+    var isInstance: Bool
+    var identifier: String
+    var parameters: [String]
+    var returnType: String
+  }
+  
+  internal var signatureStandin: SignatureStandin {
+    var parameters = [String]()
+    for parameter in signature.input.parameterList {
+      parameters.append(parameter.firstName.text + ":" + (parameter.type.genericSubstitution(genericParameterClause?.genericParameterList) ?? "" ))
+    }
+    let returnType = signature.output?.returnType.genericSubstitution(genericParameterClause?.genericParameterList) ?? "Void"
+    return SignatureStandin(isInstance: isInstance, identifier: identifier.text, parameters: parameters, returnType: returnType)
+  }
+  
+  internal func isEquivalent(to other: FunctionDeclSyntax) -> Bool {
+    return signatureStandin == other.signatureStandin
+  }
+  
+}
+
+extension TypeSyntax {
+  
+  internal var identifier: String? {
+    for token in tokens(viewMode: .all) {
+      switch token.tokenKind {
+      case .identifier(let identifier):
+        return identifier
+      default:
+        break
+      }
+    }
+    return nil
+  }
+  
+}
+
+extension DeclGroupSyntax {
+  
+  internal func hasMemberFunction(equvalentTo other: FunctionDeclSyntax) -> Bool {
+    for member in memberBlock.members {
+      if let function = member.as(MemberDeclListItemSyntax.self)?.decl.as(FunctionDeclSyntax.self) {
+        if function.isEquivalent(to: other) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+  
+  internal func hasMemberProperty(equivalentTo other: VariableDeclSyntax) -> Bool {
+    for member in memberBlock.members {
+      if let variable = member.as(MemberDeclListItemSyntax.self)?.decl.as(VariableDeclSyntax.self) {
+        if variable.isEquivalent(to: other) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+  
+  internal func addIfNeeded(_ decl: DeclSyntax?, to declarations: inout [DeclSyntax]) {
+    guard let decl else { return }
+    if let fn = decl.as(FunctionDeclSyntax.self) {
+      if !hasMemberFunction(equvalentTo: fn) {
+        declarations.append(decl)
+      }
+    } else if let property = decl.as(VariableDeclSyntax.self) {
+      if !hasMemberProperty(equivalentTo: property) {
+        declarations.append(decl)
+      }
+    } else if let `struct` = decl.as(StructDeclSyntax.self) {
+      if !hasMemberStruct(equivalentTo: `struct`) {
+        declarations.append(decl)
+      }
+    }
+  }
+  
+  internal var isClass: Bool {
+    return self.is(ClassDeclSyntax.self)
+  }
+  
+  internal var isActor: Bool {
+    return self.is(ActorDeclSyntax.self)
+  }
+  
+  internal var isEnum: Bool {
+    return self.is(EnumDeclSyntax.self)
+  }
+  
+}
--- Sources/COWMacros/SyntaxExtensions.swift
+++ Sources/COWMacros/SyntaxExtensions.swift
@@ -35,16 +35,6 @@ internal struct COWStoragePropertyDescriptor {
 
 extension StructDeclSyntax {
   
-  internal func hasMacroApplication(_ name: String) -> Bool {
-    guard let attributes else {
-      return false
-    }
-    for each in attributes where each.hasName(name) {
-      return true
-    }
-    return false
-  }
-  
   internal var copyOnWriteStorageName: TokenSyntax? {
     guard case .attribute(let attribute) = attributes?.first else {
       return nil
@@ -103,165 +93,7 @@ extension VariableDeclSyntax {
     return !info.isMarkedExcluded
   }
   
-  internal var identifierPattern: IdentifierPatternSyntax? {
-    bindings.first?.pattern.as(IdentifierPatternSyntax.self)
-  }
-  
-  internal var identifier: TokenSyntax? {
-    identifierPattern?.identifier
-  }
-  
-  internal func hasMacroApplication(_ name: String) -> Bool {
-    guard let attributes else {
-      return false
-    }
-    for each in attributes where each.hasName(name) {
-      return true
-    }
-    return false
-  }
-  
-  internal func firstMacroApplication(_ name: String) -> AttributeSyntax? {
-    guard let attributes else {
-      return nil
-    }
-    for each in attributes where each.hasName(name) {
-      switch each {
-      case .attribute(let attrSyntax):
-        return attrSyntax
-      case .ifConfigDecl:
-        break
-      }
-    }
-    return nil
-  }
-  
-  internal var isInstance: Bool {
-    if let modifiers {
-      for modifier in modifiers {
-        for token in modifier.tokens(viewMode: .all) {
-          if token.tokenKind == .keyword(.static) || token.tokenKind == .keyword(.class) {
-            return false
-          }
-        }
-      }
-    }
-    return true
-  }
-  
-  internal func isEquivalent(to other: VariableDeclSyntax) -> Bool {
-    if isInstance != other.isInstance {
-      return false
-    }
-    return identifier?.text == other.identifier?.text
-  }
-  
 }
-
-extension PatternBindingSyntax {
-  
-  internal func storagePropertyDescritor(_ keyword: TokenSyntax) -> COWStoragePropertyDescriptor? {
-    guard let identPattern = pattern.as(IdentifierPatternSyntax.self),
-          let initializer = initializer else {
-      return nil
-    }
-    
-    return COWStoragePropertyDescriptor(
-      keyword: keyword,
-      name: identPattern.identifier,
-      type: typeAnnotation?.type,
-      initializer: initializer.value
-    )
-  }
-  
-}
-
-extension TypeSyntax {
-  
-  internal func genericSubstitution(_ parameters: GenericParameterListSyntax?) -> String? {
-    var genericParameters = [String : TypeSyntax?]()
-    if let parameters {
-      for parameter in parameters {
-        genericParameters[parameter.name.text] = parameter.inheritedType
-      }
-    }
-    var iterator = self.asProtocol(TypeSyntaxProtocol.self).tokens(viewMode: .sourceAccurate).makeIterator()
-    guard let base = iterator.next() else {
-      return nil
-    }
-    
-    if let genericBase = genericParameters[base.text] {
-      if let text = genericBase?.identifier {
-        return "some " + text
-      } else {
-        return nil
-      }
-    }
-    var substituted = base.text
-    
-    while let token = iterator.next() {
-      switch token.tokenKind {
-      case .leftAngle:
-        substituted += "<"
-      case .rightAngle:
-        substituted += ">"
-      case .comma:
-        substituted += ","
-      case .identifier(let identifier):
-        let type: TypeSyntax = "\(raw: identifier)"
-        guard let substituedType = type.genericSubstitution(parameters) else {
-          return nil
-        }
-        substituted += substituedType
-        break
-      default:
-        // ignore?
-        break
-      }
-    }
-    
-    return substituted
-  }
-  
-}
-
-extension FunctionDeclSyntax {
-  
-  internal var isInstance: Bool {
-    if let modifiers {
-      for modifier in modifiers {
-        for token in modifier.tokens(viewMode: .all) {
-          if token.tokenKind == .keyword(.static) || token.tokenKind == .keyword(.class) {
-            return false
-          }
-        }
-      }
-    }
-    return true
-  }
-  
-  internal struct SignatureStandin: Equatable {
-    var isInstance: Bool
-    var identifier: String
-    var parameters: [String]
-    var returnType: String
-  }
-  
-  internal var signatureStandin: SignatureStandin {
-    var parameters = [String]()
-    for parameter in signature.input.parameterList {
-      parameters.append(parameter.firstName.text + ":" + (parameter.type.genericSubstitution(genericParameterClause?.genericParameterList) ?? "" ))
-    }
-    let returnType = signature.output?.returnType.genericSubstitution(genericParameterClause?.genericParameterList) ?? "Void"
-    return SignatureStandin(isInstance: isInstance, identifier: identifier.text, parameters: parameters, returnType: returnType)
-  }
-  
-  internal func isEquivalent(to other: FunctionDeclSyntax) -> Bool {
-    return signatureStandin == other.signatureStandin
-  }
-  
-}
-
 
 extension AttributeListSyntax.Element {
   
@@ -289,18 +121,20 @@ extension AttributeSyntax {
   
 }
 
-extension TypeSyntax {
+extension PatternBindingSyntax {
   
-  internal var identifier: String? {
-    for token in tokens(viewMode: .all) {
-      switch token.tokenKind {
-      case .identifier(let identifier):
-        return identifier
-      default:
-        break
-      }
+  internal func storagePropertyDescritor(_ keyword: TokenSyntax) -> COWStoragePropertyDescriptor? {
+    guard let identPattern = pattern.as(IdentifierPatternSyntax.self),
+          let initializer = initializer else {
+      return nil
     }
-    return nil
+    
+    return COWStoragePropertyDescriptor(
+      keyword: keyword,
+      name: identPattern.identifier,
+      type: typeAnnotation?.type,
+      initializer: initializer.value
+    )
   }
   
 }
@@ -383,28 +217,6 @@ extension AttributeSyntax.Argument {
 
 extension DeclGroupSyntax {
   
-  internal func hasMemberFunction(equvalentTo other: FunctionDeclSyntax) -> Bool {
-    for member in memberBlock.members {
-      if let function = member.as(MemberDeclListItemSyntax.self)?.decl.as(FunctionDeclSyntax.self) {
-        if function.isEquivalent(to: other) {
-          return true
-        }
-      }
-    }
-    return false
-  }
-  
-  internal func hasMemberProperty(equivalentTo other: VariableDeclSyntax) -> Bool {
-    for member in memberBlock.members {
-      if let variable = member.as(MemberDeclListItemSyntax.self)?.decl.as(VariableDeclSyntax.self) {
-        if variable.isEquivalent(to: other) {
-          return true
-        }
-      }
-    }
-    return false
-  }
-  
   internal func hasMemberStruct(equivalentTo other: StructDeclSyntax) -> Bool {
     for member in memberBlock.members {
       if let `struct` = member.as(MemberDeclListItemSyntax.self)?.decl.as(StructDeclSyntax.self) {
@@ -416,37 +228,8 @@ extension DeclGroupSyntax {
     return false
   }
   
-  internal func addIfNeeded(_ decl: DeclSyntax?, to declarations: inout [DeclSyntax]) {
-    guard let decl else { return }
-    if let fn = decl.as(FunctionDeclSyntax.self) {
-      if !hasMemberFunction(equvalentTo: fn) {
-        declarations.append(decl)
-      }
-    } else if let property = decl.as(VariableDeclSyntax.self) {
-      if !hasMemberProperty(equivalentTo: property) {
-        declarations.append(decl)
-      }
-    } else if let `struct` = decl.as(StructDeclSyntax.self) {
-      if !hasMemberStruct(equivalentTo: `struct`) {
-        declarations.append(decl)
-      }
-    }
-  }
-  
   internal var isStruct: Bool {
     return self.is(StructDeclSyntax.self)
-  }
-  
-  internal var isClass: Bool {
-    return self.is(ClassDeclSyntax.self)
-  }
-  
-  internal var isActor: Bool {
-    return self.is(ActorDeclSyntax.self)
-  }
-  
-  internal var isEnum: Bool {
-    return self.is(EnumDeclSyntax.self)
   }
   
 }
